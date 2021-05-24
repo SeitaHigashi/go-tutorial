5f105fc71827725b62e82317e6390e8a7163facd
SeitaHigashi <higashi110902@gmail.com>
2021-05-24 23:00:02 +0900

    Return greetings for multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -7,12 +7,32 @@ import (
   "time"
 )
 
+// Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
   if name == "" {
     return "", errors.New("empty name")
   }
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// Hellos returns a map that associated each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+  // A map to associate names with messages.
+  messages := make(map[string]string)
+  // Loop through the received slice of names, calling
+  // the Hello function to get a message for each name.
+  for _, name := range names {
+    message, err := Hello(name)
+    if err != nil {
+      return nil, err
+    }
+    // In the map, associate the retrieved message with
+    // the name.
+    messages[name] = message
+  }
+  return messages, nil
 }
 
 // init sets initial value for variables used in the function.
